c55967a427b1dbd493e168909e6029f0fbfdd12e
Chansu Kim <ckstn0777@naver.com>
2023-05-09 14:23:08 +0900

    feat: touch-action으로 기본 pinch-zoom 막기 추가

--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,9 @@ import "./style.css";
 function preventBrowserZoom() {
   function listener(event: TouchEvent) {
     // 핀치 줌의 경우 두 개 이상의 이벤트가 발생한다
-    console.log(event);
     if (event.touches.length > 1) {
-      event.preventDefault(); // preventDefault를 사용하려면 passive를 false로 해줘야겠군. 음... 그런 의미였구나
+      // preventDefault를 사용하려면 passive를 false로 해줘야 한다
+      event.preventDefault();
     }
   }
 
